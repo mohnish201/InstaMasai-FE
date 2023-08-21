a0b551c8335cab53d42a59b94e268ab992acbbe7
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-08-21 15:11:47 +0530

    tried with deploy BE link not working

--- my-app/src/Pages/Login.jsx
+++ my-app/src/Pages/Login.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from 'axios'
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e)=> {
+
+    const data={email,password}
+    e.preventDefault()
+    axios
+    .post("http://localhost:8000/users/login", data)
+    .then((res) => console.log(res.data))
+    .catch((err) => console.log(err));
+};
   return (
     <div>
-
-        <form>
-
-            <input type="text" placeholder='Name'  />
-            <input type="text" placeholder='Email'  />
-            <input type="text" placeholder='Gender'  />
-            <input type="text" placeholder='password'  />
-            <input type="text" placeholder='Age'  />
-            <input type="text" placeholder='City'  />
-            <input type="text" placeholder='Marital Status'  />
-        </form>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+        />
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- my-app/src/Pages/Signup.jsx
+++ my-app/src/Pages/Signup.jsx
@@ -1,21 +1,88 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+const initState = {
+  name: "",
+  email: "",
+  gender: "",
+  password: "",
+  age: "",
+  city: "",
+  is_married: "",
+};
 const Signup = () => {
+  const [data, setData] = useState(initState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8000/users/register", data)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  const { name, age, email, gender, city, is_married, password } = data;
   return (
     <div>
-         <form>
-
-<input type="text" placeholder='Name'  /> <br />
-<input type="text" placeholder='Email'  /> <br />
-<input type="text" placeholder='Gender'  /><br />
-<input type="text" placeholder='password'  /><br />
-<input type="text" placeholder='Age'  /><br />
-<input type="text" placeholder='City'  /><br />
-<input type="text" placeholder='Marital Status'/><br />
-<input type={"submit"} value="submit" />
-</form>
+      <form onSubmit={handleSignup}>
+        <input type="text" placeholder="Name" name="name" value={name} onChange={handleChange} /> <br />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <input
+          type="text"
+          placeholder="Gender"
+          name="gender"
+          value={gender}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Age"
+          name="age"
+          value={age}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="City"
+          name="city"
+          value={city}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Marital Status"
+          name="is_married"
+          value={is_married}
+          onChange={handleChange}
+        />
+        <br />
+        <input type={"submit"} value="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
