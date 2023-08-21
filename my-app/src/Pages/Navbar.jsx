b1d0d505c84de77dcecd493ddb8172ee56715f77
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-08-21 14:26:08 +0530

    Navbar Created

--- my-app/src/Pages/Navbar.jsx
+++ my-app/src/Pages/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        padding: "10px",
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">SignUp</Link>
+      <Link to="/posts">Posts</Link>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
